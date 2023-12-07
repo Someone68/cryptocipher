07ccd5672595a49dad5ea59bd464013f7ef516d2
Someone68 <97077158+Someone68@users.noreply.github.com>
2023-12-07 02:38:25 +0000

    initial commit (kinda)

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  else console.log(`Server listening on port ${3000}`);
+});
