39ca495521243a1187ed7ca494f18b1016f06403
Someone68 <97077158+Someone68@users.noreply.github.com>
2023-12-14 02:23:19 +0000

    add vercel support and remove .html

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require("express");
-const app = express();
 const path = require("path");
-const PORT = 3000;
+const app = express();
+const port = 3000;
+const fs = require("fs");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.get("/", (req, res) => {
-  res.sendStatus(200);
+	response.sendStatus(200);
 });
 
-app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  else console.log(`Server listening on port ${3000}`);
+// Middleware to remove .html extension from URLs
+app.use((req, res, next) => {
+	const filePath = path.join(__dirname, "public", `${req.path}`);
+	if (req.path !== "/" && fs.existsSync(`${filePath}.html`)) {
+		req.url = `${req.path}.html`;
+	}
+	next();
+});
+
+// Route to serve .html files
+app.get("/*.html", (req, res, next) => {
+	const filePath = path.join(__dirname, "public", `${req.path}`);
+	if (fs.existsSync(`${filePath}`)) {
+		res.sendFile(`${filePath}`);
+	} else {
+		next();
+	}
+});
+
+// 404 handler
+app.use((req, res) => {
+	res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+
+app.listen(port, () => {
+	console.log(`Server listening on port 3000`);
 });
